64b1c9b526585f6c29fd59724082906ab67127af
Srijan <srijanksah02@gmail.com>
2023-08-11 00:39:54 +0530

    home

--- portal/src/pages/home.jsx
+++ portal/src/pages/home.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-
+import portal from '../assests/portal.gif';
+import './styles/home.css';
 
 
 const home = () => {
     return (
-        <div>
-            
+        
+        <div class="container">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Attendance</a></li>
+			<li><a href="#">Grade</a></li>
+			<li><a href="#">News</a></li>
+			<li><a href="#">Login</a></li>
+		</ul>
+	</div>
+
 
             
-        </div>
+      
     )
 }
 
