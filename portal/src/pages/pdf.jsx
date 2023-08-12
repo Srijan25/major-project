73515bd5e6e74727eaaca09db6fcd0f39f55213d
Srijan <srijanksah02@gmail.com>
2023-08-12 23:42:05 +0530

    pdf

--- portal/src/pages/pdf.jsx
+++ portal/src/pages/pdf.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
-import './styles/pdf.css';
-
+import './styles/pdf.css'
 
 const Pdf = () => {
-    return (
-        <>
-        <input type="file" id="pdf-upload" accept="application/pdf" />
-        <button id="zoom-in">Zoom In</button>
-        <div id="zoom-percent">60</div>
-        <button id="zoom-out">Zoom Out</button>
-        <button id="zoom-reset">Reset Zoom</button>
-        <div id="pages" />
+  return (
+    <>
+    <div className="formbold-main-wrapper">
+  {/* Author: FormBold Team */}
+  {/* Learn More: https://formbold.com */}
+  <div className="formbold-form-wrapper">
+    <div className="mb-6 pt-4">
+      <label className="formbold-form-label formbold-form-label-2">
+        Upload File
+      </label>
+      <div className="formbold-mb-5 formbold-file-input">
+        <input type="file" name="file" id="file" />
+        <label htmlFor="file">
+          <div>
+            <span className="formbold-drop-file"> Drop files here </span>
+            <span className="formbold-or"> Or </span>
+            <span className="formbold-browse"> Browse </span>
+          </div>
+        </label>
+      </div>
+      <div>
+        <button className="formbold-btn w-full">Send File</button>
+      </div>
+    </div>
+  </div>
+</div>
+</>
 
-      </>
-      
-    )
+  )
 }
 
 export default Pdf
