f4dd1f67659be90f639960ff945d493cf2e13617
Srijan <srijanksah02@gmail.com>
2023-08-12 19:29:10 +0530

    footer

--- portal/src/pages/footer.jsx
+++ portal/src/pages/footer.jsx
@@ -3,45 +3,73 @@ import './styles/footer.css';
 
 function Footer() {
   return (
-    <div className="footer-clean">
-      <footer>
-        <div className="footer-container">
-          <div className="row justify-content-center">
-            <div className="col-sm-4 col-md-3 item">
-              <h3>Services</h3>
-              <ul>
-                <li><a href="#">Web design</a></li>
-                <li><a href="#">Development</a></li>
-                <li><a href="#">Hosting</a></li>
-              </ul>
-            </div>
-            <div className="col-sm-4 col-md-3 item">
-              <h3>About</h3>
-              <ul>
-                <li><a href="#">Company</a></li>
-                <li><a href="#">Team</a></li>
-                <li><a href="#">Legacy</a></li>
-              </ul>
-            </div>
-            <div className="col-sm-4 col-md-3 item">
-              <h3>Careers</h3>
-              <ul>
-                <li><a href="#">Job openings</a></li>
-                <li><a href="#">Employee success</a></li>
-                <li><a href="#">Benefits</a></li>
-              </ul>
-            </div>
-            <div className="col-lg-3 item social">
-              <a href="#"><i className="icon ion-social-facebook"></i></a>
-              <a href="#"><i className="icon ion-social-twitter"></i></a>
-              <a href="#"><i className="icon ion-social-snapchat"></i></a>
-              <a href="#"><i className="icon ion-social-instagram"></i></a>
-              <p className="copyright">Company Name © {new Date().getFullYear()}</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+    <>
+  {/*-------- Footer HTML Starts -------*/}
+  <div className="footer">
+    <div className="container">
+      <div className="footer-sect">
+        <h2>Best Institute For Education</h2>
+        <p>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+          it over 2000 years old. Richard McClintock.
+        </p>
+        <img src="images/icon-fb.png" />
+        <img src="images/icon-tw.png" />
+        <img src="images/icon-in.png" />
+        <img src="images/icon-li.png" />
+      </div>
+      <div className="footer-sect">
+        <h2>Quick Links</h2>
+        <ul className="footer-menu">
+          <li>
+            <a href="#"> &gt; Home</a>
+          </li>
+          <li>
+            <a href="#"> &gt; About us</a>
+          </li>
+          <li>
+            <a href="#"> &gt; Tutorial</a>
+          </li>
+          <li>
+            <a href="#"> &gt; Event</a>
+          </li>
+          <li>
+            <a href="#"> &gt; Gallery</a>
+          </li>
+          <li>
+            <a href="#"> &gt; Press Release</a>
+          </li>
+          <li>
+            <a href="#"> &gt; Courses</a>
+          </li>
+          <li>
+            <a href="#"> &gt; Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-sect">
+        <h2>Contact Info</h2>
+        <ul className="footer-contact">
+          <li>
+            <b>Email :</b> reply@bestinstituteeducation.com{" "}
+          </li>
+          <li>
+            <b>Call :</b> +91 -123 456 7890{" "}
+          </li>
+          <li>
+            <b>Fax :</b> +91 -123 4567890{" "}
+          </li>
+          <li>
+            <b>Website :</b> https://www.dezven.com{" "}
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
+  {/*-------- Footer HTML Ends -------*/}
+</>
+
   );
 }
 
